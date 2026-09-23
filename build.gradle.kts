0cc60b9b2763125a063b4c368ea6553ede5490f8
xjxksfd-code <xjxksfd@gmail.com>
2026-09-24 03:10:08 +0800

    fix(ci): drop aliyun mirrors, use google()+mavenCentral() for CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 buildscript {
     repositories {
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
         google()
         mavenCentral()
     }
